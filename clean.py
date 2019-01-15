e08d4d5bfe53e49b43a8b5bad994d069f5690d3e
nverdiguel <nverdiguel@knights.ucf.edu>
2019-01-15 14:21:00 -0500

    fill out modules

--- clean.py
+++ clean.py
@@ -1,4 +1,30 @@
 # Move utility functions for data cleaning here
 
+def clean_paths(row):
+    return os.path.basename(row['oldfractal'])
+
+def normalize(row):
+    study = row['study']
+    val = row[key]
+    group_mean = df.groupby('study').mean().loc[study,key]
+    group_std = df.groupby('study').std().loc[study,key]
+    zval = (val - group_mean) / group_std
+    return zval
+
+def exclude_3sd(row):
+    ssid,val = row['ssid'],row['val_estdiff_valid']
+    mean,bound = subj_means[ssid],subj_3sd[ssid]
+    diff = abs(val - mean)
+    if diff < bound:
+        return(val)
+    else: return(np.nan)
+    
+def normalize_frame(row):
+    ssid,val = row.name,row['valError_3sd']
+    ssid = str(ssid)
+    if ssid.startswith('2'):
+        val = val * -1
+    return(val)
+
 def __init__(self):
     pass
--- transform.py
+++ transform.py
@@ -1,4 +1,51 @@
 # Move utility functions for data transformation here
 
+def sum_oscillations(row):
+    if row['ia-id'] in stock_ia_list:
+        return sum([row['fsa-ia-02'],row['fsa-ia-25'],row['fsa-ia-27']])
+    elif row['ia-id'] in bond_ia_list:
+        return sum([row['fsa-ia-01'],row['fsa-ia-24'],row['fsa-ia-26']])
+
+def oscillation_rate(row):
+    return float(row['oscillations'] / row['choicert'])
+
+def score_relative_stress(row):
+    if row[4] < 5: return -1
+    if row[4] > 5: return 1
+    if row[4] == 5: return 0
+    
+def numeralize_sleep(row):
+    try:
+        if isinstance(row[2],basestring) and '-' in row[2]:
+            values = row[2].split('-')
+            return np.average([float(values[0]),float(values[1])])
+        else:
+            return float(row[2])
+    except ValueError as e:
+        print(e)
+        return(row[2])
+    
+def score_relative_sleep(row):
+    try:
+        if float(row[1]) < float(row[2]): return -1
+        if float(row[1]) > float(row[2]): return 1
+        if float(row[1]) == float(row[2]): return 0
+    except ValueError as e:
+        print(e)
+        return('NaN')
+    
+def convert_prob(row):
+    good_prob = row['TrueProbGood']
+    bad_prob = (1.00 - good_prob)
+    return bad_prob
+
+def convert_check(row):
+    subj_resp = row['ProbGood']
+    bad_prob = row['TrueProbGood']
+    old_check = row['EstWithinRange?']
+    new_check = 1 if (bad_prob+.05) >= (subj_resp*.01) >= (bad_prob-.05) else 0
+    return new_check
+
+
 def __init__(self):
     pass
